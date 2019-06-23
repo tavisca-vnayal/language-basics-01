614dd77cc05a9958b5fc0d039dd68a4834009f12
tavisca-vnayal <vnayal@tavisca.in>
2019-06-23 14:47:40 +0530

    Solution for Language Basics 01 Problem Statement

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,6 +23,57 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
+            string firstNumber = equation.Split("*")[0];
+            string secondNumber = equation.Split("*")[1].Split("=")[0];
+            string observedResult = equation.Split("=")[1];
+
+            bool isFirstNumberNumeric = Int32.TryParse(firstNumber, out int intFirstNumber);
+            bool isSecondNumberNumeric = Int32.TryParse(secondNumber, out int intSecondNumber);
+            bool isObservedResultNumeric = Int32.TryParse(observedResult, out int intObservedResult);
+
+            string correctEquation = "";
+
+            if(isFirstNumberNumeric == false)
+            {
+                if(intObservedResult % intSecondNumber == 0)
+                {
+                    string newFirstNumber = Convert.ToString(intObservedResult / intSecondNumber);
+                    correctEquation += newFirstNumber + "*" + secondNumber + "=" + observedResult;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            if (isSecondNumberNumeric == false)
+            {
+                if (intObservedResult % intFirstNumber == 0)
+                {
+                    string newSecondNumber = Convert.ToString(intObservedResult / intFirstNumber);
+                    correctEquation += firstNumber + "*" + newSecondNumber + "=" + observedResult;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            if (isObservedResultNumeric == false)
+            {
+                string expectedResult = Convert.ToString(intFirstNumber * intSecondNumber);
+                correctEquation += firstNumber + "*" + secondNumber + "=" + expectedResult;
+            }
+
+            if(equation.Length != correctEquation.Length)
+            {
+                return -1;
+            }
+            for(int i=0;i<correctEquation.Length;i++)
+            {
+                if (correctEquation[i] != equation[i] && equation[i] == '?')
+                {
+                    return (correctEquation[i] - '0');
+                }
+            }
             throw new NotImplementedException();
         }
     }
